f85cb4eaefd2c93e19522938d400a67a173232f8
Alex Jago <4475543+alexjago@users.noreply.github.com>
2023-12-02 23:18:50 +1000

    day 02 complete

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -23,7 +23,7 @@ fn main() -> Result<()> {
     let infile = read_to_string(opts.infile)?;
 
     println!("Part 1:\n{}", part_1(&infile)?);
-    // println!("Part 2:\n{}", part_2(&infile)?);
+    println!("Part 2:\n{}", part_2(&infile)?);
 
     Ok(())
 }
@@ -74,6 +74,17 @@ const LIMITS_1: Handful = Handful {
     blue: 14,
 };
 
+fn parse_line(input: &str) -> (u32, Vec<Handful>) {
+    let game_tag = tag("Game ");
+    let game_tot = preceded(game_tag, number);
+    let dice = separated_pair(number, multispace1, colour);
+    let handful = map(separated_list1(tag(", "), dice), Handful::from);
+    let set_list = separated_list1(tag("; "), handful);
+    let mut full_line = separated_pair(game_tot, tag(": "), set_list);
+    full_line(input).finish().unwrap().1
+}
+
+/// Element-wise LEQ to constant
 fn part_1(input: &str) -> Result<u32> {
     // Game format
     // Game (\d+): (((\d) (blue|green|red),?\s?))(; ((\d) (blue|green|red),?\s?))*
@@ -94,23 +105,42 @@ fn part_1(input: &str) -> Result<u32> {
         if l.is_empty() {
             continue;
         }
-        let game_tag = tag("Game ");
-        let game_tot = preceded(game_tag, number);
-        let dice = separated_pair(number, multispace1, colour);
-        let handful = map(separated_list1(tag(", "), dice), Handful::from);
-        let set_list = separated_list1(tag("; "), handful);
-        let mut full_line = separated_pair(game_tot, tag(": "), set_list);
-        let (id, hands) = full_line(l).finish().unwrap().1;
+        let (id, hands) = parse_line(l);
         if hands
             .iter()
             .all(|h| h.red <= LIMITS_1.red && h.green <= LIMITS_1.green && h.blue <= LIMITS_1.blue)
         {
             total += id;
-            print!("[Y] ");
+            // print!("[Y] ");
         } else {
-            print!("[N] ");
+            // print!("[N] ");
         }
-        println!("{id}, {hands:?}");
+        // println!("{id}, {hands:?}");
+    }
+
+    Ok(total)
+}
+
+/// Element-wise min
+fn part_2(input: &str) -> Result<u32> {
+    let mut total = 0;
+    for l in input.lines() {
+        if l.is_empty() {
+            continue;
+        }
+        let (_, hands) = parse_line(l);
+
+        let min_hand = hands
+            .into_iter()
+            .reduce(|acc, e| Handful {
+                red: acc.red.max(e.red),
+                green: acc.green.max(e.green),
+                blue: acc.blue.max(e.blue),
+            })
+            .unwrap();
+
+        let power = min_hand.red * min_hand.green * min_hand.blue;
+        total += power;
     }
 
     Ok(total)
@@ -120,15 +150,19 @@ fn part_1(input: &str) -> Result<u32> {
 mod test {
     use super::*;
 
-    #[test]
-    fn test_example_part_1() {
-        let eg_1 = r"
+    const EG_1: &str = r"
 Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
-        assert_eq!(part_1(eg_1).unwrap(), 8);
+    #[test]
+    fn test_example_part_1() {
+        assert_eq!(part_1(EG_1).unwrap(), 8);
+    }
+    #[test]
+    fn test_example_part_2() {
+        assert_eq!(part_2(EG_1).unwrap(), 2286);
     }
 }
