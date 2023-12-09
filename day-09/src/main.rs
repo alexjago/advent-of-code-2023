eddf8022318ae28d9f5404eca675280e4a57bcc9
Alex Jago <4475543+alexjago@users.noreply.github.com>
2023-12-09 16:05:23 +1000

    day 09 part 2

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -17,7 +17,7 @@ fn main() -> Result<()> {
     let infile = read_to_string(opts.infile)?;
 
     println!("Part 1:\n{}", part_1(&infile));
-    println!("Part 2:\n{}", part_2(&infile)?);
+    println!("Part 2:\n{}", part_2(&infile));
 
     Ok(())
 }
@@ -67,8 +67,51 @@ fn part_1(infile: &str) -> isize {
         })
         .sum()
 }
-fn part_2(infile: &str) -> Result<usize> {
-    todo!()
+
+/// Sequence of differences
+fn part_2(infile: &str) -> isize {
+    infile
+        .lines()
+        .filter(|s| !s.is_empty())
+        .map(|line| {
+            println!("\n{line}");
+            let mut diffs: HashMap<usize, Vec<isize>> = HashMap::new();
+
+            diffs.insert(
+                0,
+                line.split_whitespace()
+                    .filter_map(|s| s.parse().ok())
+                    .collect(),
+            );
+
+            let mut curr_level = 0_usize;
+
+            while diffs
+                .get(&curr_level)
+                .is_some_and(|sx| sx.iter().any(|&x| x != 0))
+            {
+                // Take successive differences
+                let next_level = diffs
+                    .get(&curr_level)
+                    .unwrap()
+                    .iter()
+                    .tuple_windows()
+                    .map(|(p, n)| n - p)
+                    .collect();
+                curr_level += 1;
+                diffs.insert(curr_level, next_level);
+            }
+
+            let mut nv = 0;
+            let mut cv = 0;
+            for c in (0..curr_level).rev() {
+                cv = nv;
+                nv = *diffs.get(&c).unwrap().first().unwrap_or(&0_isize) - cv;
+                println!("level: {c}\tprevious: {cv}\tnext: {nv}");
+            }
+            nv
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -86,6 +129,6 @@ mod test {
 
     #[test]
     fn part_2_example() {
-        assert_eq!(part_2(EXAMPLE_1).unwrap(), todo!());
+        assert_eq!(part_2(EXAMPLE_1), 2);
     }
 }
