30acd657b3f62f6085ec0d17080c1b7e1942fa31
Alex Jago <4475543+alexjago@users.noreply.github.com>
2023-12-10 18:53:59 +1000

    day 10

--- new-day.sh
+++ new-day.sh
@@ -8,13 +8,15 @@ printf -v newday -- 'day-%02d' "$daynum"
 
 cp -R template "$newday"
 
-sed -i -e "s/template/$newday/" "$newday/Cargo.toml"
+sed -i '.bak' -e "s/template/$newday/" "$newday/Cargo.toml"
 
 printf -v newdayversion -- 's/^version =.*$/version = "0.%d.0"/' "$daynum"
 
 printf -v newtoday -- 's|^path = "day-.*$|path = "%s/src/main.rs"|' "$newday"
 
-sed -i -e "$newdayversion" -e "$newtoday" Cargo.toml
+sed -i '.bak' -e "$newdayversion" -e "$newtoday" Cargo.toml
+
+rm "Cargo.toml.bak" "$newday/Cargo.toml.bak"
 
 git add "Cargo.toml" "$newday"
-git commit -am "day $daynum"
+git commit -am "day-$daynum"
