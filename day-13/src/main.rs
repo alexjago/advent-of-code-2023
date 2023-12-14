aeff8769131c734caf112616739708dd7e2d623f
Alex Jago <4475543+alexjago@users.noreply.github.com>
2023-12-14 15:12:26 +1000

    day 13 part 2

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -37,11 +37,8 @@ In the above example, the only shared reflection point in all three rows is at 2
 
 use anyhow::Result;
 use clap::Parser;
-use mapgrid::{Coord, Grid};
-use nom;
-use regex;
-use std::{collections::HashSet, fs::read_to_string};
-use strum;
+
+use std::fs::read_to_string;
 
 #[derive(Parser)]
 pub struct Opts {
@@ -69,8 +66,16 @@ fn part_1(infile: &str) -> usize {
     infile
         .split("\n\n")
         .map(|pat| {
-            let vert = find_reflection_col(pat).map(|x| x + 1).unwrap_or(0);
-            let horz = find_reflection_row(pat).map(|x| x + 1).unwrap_or(0);
+            let vert = find_reflections_col(pat)
+                .iter()
+                .map(|x| x + 1)
+                .next()
+                .unwrap_or(0);
+            let horz = find_reflections_row(pat)
+                .iter()
+                .map(|x| x + 1)
+                .next()
+                .unwrap_or(0);
 
             // println!("{pat}\n{horz}\t{vert}");
 
@@ -81,7 +86,7 @@ fn part_1(infile: &str) -> usize {
 
 /// Find a reflection by columns ("vertical")
 /// newline-delimited strings are inherently row-major
-fn find_reflection_col(input: &str) -> Option<usize> {
+fn find_reflections_col(input: &str) -> Vec<usize> {
     let rows: Vec<Vec<char>> = input
         .lines()
         .filter(|s| !s.is_empty())
@@ -89,9 +94,9 @@ fn find_reflection_col(input: &str) -> Option<usize> {
         .collect();
 
     let width = rows.iter().map(|r| r.len()).min().unwrap_or(0);
-    let height = rows.len();
+    let _height = rows.len();
 
-    let mut out: Option<usize> = None;
+    let mut out: Vec<usize> = vec![];
 
     for start in 0..(width - 1) {
         let mut b = start;
@@ -105,10 +110,11 @@ fn find_reflection_col(input: &str) -> Option<usize> {
             // Have b or d hit the edge?
             if b == 0 || d >= width - 1 {
                 // println!("Hit the edges at {b} {d}")
-                if out.is_none() || (start > out.unwrap() && start < height - out.unwrap()) {
-                    // println!("Adding vertical reflection at {start}   ({b} {d})");
-                    out = Some(start);
-                }
+                // if out.is_none() || (start > out.unwrap() && start < width - out.unwrap()) {
+                // println!("Adding vertical reflection at {start}   ({b} {d})");
+                // out = Some(start);
+                // }
+                out.push(start);
                 break;
             }
             b -= 1;
@@ -120,12 +126,12 @@ fn find_reflection_col(input: &str) -> Option<usize> {
 }
 
 /// Find a reflection by rows ("horizontal")
-fn find_reflection_row(input: &str) -> Option<usize> {
+fn find_reflections_row(input: &str) -> Vec<usize> {
     // we can compare rows at a time, lol
 
     let rows: Vec<&str> = input.lines().filter(|s| !s.is_empty()).collect();
 
-    let mut out: Option<usize> = None;
+    let mut out: Vec<usize> = vec![];
     for start in 0..(rows.len() - 1) {
         let mut b = start;
         let mut d = start + 1;
@@ -134,9 +140,10 @@ fn find_reflection_row(input: &str) -> Option<usize> {
                 break;
             }
             if b == 0 || d >= rows.len() - 1 {
-                if out.is_none() || (start > out.unwrap() && start < rows.len() - out.unwrap()) {
-                    out = Some(start);
-                }
+                // if out.is_none() || (start > out.unwrap() && start < rows.len() - out.unwrap()) {
+                // out = Some(start);
+                // }
+                out.push(start);
                 break;
             }
             b -= 1;
@@ -146,8 +153,130 @@ fn find_reflection_row(input: &str) -> Option<usize> {
     out
 }
 
+/// Upon closer inspection, you discover that every mirror has exactly one smudge: exactly one . or # should be the opposite type.
+///
+/// In each pattern, you'll need to locate and fix the smudge that causes a different reflection line to be valid.
+/// (The old reflection line won't necessarily continue being valid after the smudge is fixed.)
 fn part_2(infile: &str) -> usize {
-    todo!()
+    // I think we can probably just do this exhaustively
+    infile
+        .split("\n\n")
+        .map(|pat| {
+            let vert_orig = find_reflections_col(pat).iter().map(|x| x + 1).next();
+            let horz_orig = find_reflections_row(pat).iter().map(|x| x + 1).next();
+
+            exh_unsmudge(pat, vert_orig, horz_orig)
+        })
+        .sum()
+}
+fn exh_unsmudge(pat: &str, vert_orig: Option<usize>, horz_orig: Option<usize>) -> usize {
+    //! There should be exactly one unsmudge op that will result in a different line becoming valid.
+    //!     (vert_new, horz_new) != (vert_orig, horz_orig) && (vert_new, horz_new) != (None, None)
+    //! For OUTPUT we only care about the new line (either added or modified)
+    //!
+    //! Now, if there are multiple available reflections, one must take priority.
+    //! In part 1 we dealt with this (but didn't need to) in find_reflections_*
+    //! Now we must deal with it here.
+    //! Actually, it's not so much that one must take priority, it's that we need the new one
+    //! TODO: we still might need to filter for priority if there are two possible new ones
+
+    let rows: Vec<Vec<char>> = pat
+        .lines()
+        .filter(|s| !s.is_empty())
+        .map(|l| l.chars().collect())
+        .collect();
+
+    let width = rows.iter().map(|r| r.len()).min().unwrap_or(0);
+    let height = rows.len();
+
+    for h in 0..height {
+        for w in 0..width {
+            let mut testy = rows.clone();
+            testy[h][w] = match testy[h][w] {
+                '#' => '.',
+                '.' => '#',
+                _ => unreachable!(),
+            };
+            let test_pat: String = testy
+                .into_iter()
+                .map(|v| v.iter().collect::<String>())
+                .map(|mut x| {
+                    x.push('\n');
+                    x
+                })
+                .collect();
+
+            // Apply priority
+            let vert_new = find_reflections_col(&test_pat)
+                .iter()
+                .map(|x| x + 1)
+                .find(|&x| Some(x) != vert_orig);
+            let horz_new = find_reflections_row(&test_pat)
+                .iter()
+                .map(|x| x + 1)
+                .find(|&x| Some(x) != horz_orig);
+            // println!(
+            // "({w:02}, {h:02})\tV: {vert_orig:?} -> {vert_new:?}\tH: {horz_orig:?} -> {horz_new:?}"
+            // );
+
+            // if (vert_new, horz_new) != (vert_orig, horz_orig) &&
+            if (vert_new, horz_new) != (None, None) {
+                // println!("Old pattern:\n{pat}");
+                // println!("h {:?} v {:?}", horz_orig, vert_orig);
+                // println!(
+                //     "\nNew pattern (changed ({w}, {h})):\n{}",
+                //     test_pat.trim_end()
+                // );
+                // println!("h {:?} v {:?}\n", horz_new, vert_new);
+                let mut out = 0;
+                if vert_new != vert_orig {
+                    out += vert_new.unwrap_or(0);
+                }
+                if horz_new != horz_orig {
+                    out += 100 * horz_new.unwrap_or(0);
+                }
+                return out;
+            }
+        }
+    }
+    panic!("no new reflection found for \n{pat}");
+}
+
+fn rowdiff(pat: &str) {
+    println!("linediff:");
+    for (i, (a, b)) in pat.lines().zip(pat.lines().skip(1)).enumerate() {
+        let count = a.chars().zip(b.chars()).filter(|(x, y)| x != y).count();
+        if count == 0 {
+            println!("\n{i}\t{a}\n{}\t{b}\n\n", i + 1);
+        } else {
+            print!("{i}: {count}\t");
+        }
+    }
+    println!();
+}
+
+fn coldiff(pat: &str) {
+    println!("coldiff");
+
+    let rows: Vec<Vec<char>> = pat
+        .lines()
+        .filter(|s| !s.is_empty())
+        .map(|l| l.chars().collect())
+        .collect();
+
+    let width = rows.iter().map(|r| r.len()).min().unwrap_or(0);
+
+    for i in 0..(width - 1) {
+        let a: String = rows.iter().map(|r| r[i]).collect();
+        let b: String = rows.iter().map(|r| r[i + 1]).collect();
+        let count = a.chars().zip(b.chars()).filter(|(x, y)| x != y).count();
+        if count == 0 {
+            println!("\n{i}\t{a}\n{}\t{b}\n\n", i + 1);
+        } else {
+            print!("{i}: {count}\t");
+        }
+    }
+    println!();
 }
 
 #[cfg(test)]
@@ -174,14 +303,14 @@ mod test {
 
     #[test]
     fn test_row_find() {
-        assert_eq!(find_reflection_row(EXAMPLE_1_B).unwrap() + 1, 4);
-        assert_eq!(find_reflection_row(EXAMPLE_1_A), None);
+        assert_eq!(find_reflections_row(EXAMPLE_1_B)[0] + 1, 4);
+        assert!(find_reflections_row(EXAMPLE_1_A).is_empty());
     }
 
     #[test]
     fn test_col() {
-        assert_eq!(find_reflection_col(EXAMPLE_1_A).unwrap() + 1, 5);
-        assert_eq!(find_reflection_col(EXAMPLE_1_B), None);
+        assert_eq!(find_reflections_col(EXAMPLE_1_A)[0] + 1, 5);
+        assert!(find_reflections_col(EXAMPLE_1_B).is_empty());
     }
 
     #[test]
@@ -191,6 +320,80 @@ mod test {
 
     #[test]
     fn part_2_example() {
-        assert_eq!(part_2(EXAMPLE_1_A), todo!());
+        assert_eq!(part_2(&format!("{EXAMPLE_1_A}\n\n{EXAMPLE_1_B}")), 400);
+    }
+    #[test]
+    fn part_1_real() {
+        assert_eq!(part_1(include_str!("../input.txt")), 33975);
+    }
+
+    /// Apparently, there are no smudge changes possible on this one?
+    /// What *should* happen is that there's a # at row 8 col 16 which if deleted
+    /// makes a larger reflection available
+    /// but that's not getting picked up?
+    const HMM_1: &str = r"..##...#....####.
+##.....###..#..#.
+####.#.###.######
+####...#.#.######
+..####.#.###....#
+..##.#..##...##..
+###..######......
+...####.###....#.
+##...####.#.####.
+####..####.##..##
+##.#..#.#.#.####.
+..........##.##.#
+..#...####.#.##.#";
+    #[test]
+    fn dual_reflection() {
+        assert_eq!(part_1(HMM_1), 1);
+    }
+
+    /// One # to .
+    const HMM_1_FIX: &str = r"..##...#....####.
+##.....###..#..#.
+####.#.###.######
+####...#.#.######
+..####.#.###....#
+..##.#..##...##..
+###..######......
+...####.###......
+##...####.#.####.
+####..####.##..##
+##.#..#.#.#.####.
+..........##.##.#
+..#...####.#.##.#";
+    #[test]
+    fn hmm1fix() {
+        assert_eq!(find_reflections_col(HMM_1_FIX), vec![0, 13]);
+    }
+
+    const HMM_2: &str = r"#.#.#.##.
+...##...#
+...##...#
+#.#.#.#..
+#..######
+.#.##..##
+.###.#.##
+#...####.
+#..##....
+....#.#.#
+####.#.#.
+###...#..
+.#.##...#
+##.####..
+##.####..
+.#.##...#
+###...#..";
+
+    #[test]
+    fn wtf2() {
+        println!("v: {:?}", find_reflections_col(HMM_2));
+        println!("h: {:?}", find_reflections_row(HMM_2));
+        coldiff(HMM_2);
+        rowdiff(HMM_2);
+        assert_eq!(exh_unsmudge(HMM_2, None, Some(14)), 200);
     }
 }
+
+// Pt 2: 28336 is too low for my input
