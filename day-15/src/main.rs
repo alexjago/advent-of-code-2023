e16d85fc5710c789a9b14f110b08d2cfe6603930
Alex Jago <4475543+alexjago@users.noreply.github.com>
2023-12-15 15:43:31 +1000

    day 15 part 1 and 2

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -2,9 +2,6 @@ use std::fs::read_to_string;
 
 use anyhow::Result;
 use clap::Parser;
-use nom;
-use regex;
-use strum;
 
 #[derive(Parser)]
 pub struct Opts {
@@ -23,26 +20,78 @@ fn main() -> Result<()> {
 }
 
 fn part_1(infile: &str) -> usize {
-    todo!()
+    infile.trim().split(',').map(hash).sum()
 }
+
+fn hash(input: &str) -> usize {
+    let mut out = 0;
+
+    for c in input.chars() {
+        out += c as usize;
+        out *= 17;
+        out %= 256;
+    }
+    out
+}
+
 fn part_2(infile: &str) -> usize {
-    todo!()
+    // so we're implementing a hashmap with linear probing, right?
+
+    let mut boxes: Vec<Vec<Entry>> = vec![vec![]; 256];
+
+    for lens in infile.trim().split(',') {
+        if let Some(label) = lens.strip_suffix('-') {
+            let addr = hash(label);
+            boxes[addr].retain(|f| f.key != label);
+        } else {
+            let (label, focal) = lens.split_once('=').unwrap();
+            // check if existing
+            let mut done = false;
+            let addr = hash(label);
+            for e in &mut boxes[addr] {
+                if e.key == label {
+                    e.value = focal.parse().unwrap();
+                    done = true;
+                }
+            }
+            if !done {
+                boxes[addr].push(Entry {
+                    key: label.to_string(),
+                    value: focal.parse().unwrap(),
+                });
+            }
+        }
+        // println!("{lens} :\n{boxes:?}");
+    }
+
+    let mut total = 0;
+    for (box_num, boxn) in boxes.iter().enumerate() {
+        for (slot_num, lens) in boxn.iter().enumerate() {
+            total += (1 + box_num) * (1 + slot_num) * lens.value;
+        }
+    }
+    total
+}
+
+#[derive(Clone, Debug)]
+struct Entry {
+    key: String,
+    value: usize,
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    const EXAMPLE_1: &str = r"
-";
+    const EXAMPLE_1: &str = r"rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
 
     #[test]
     fn part_1_example() {
-        assert_eq!(part_1(EXAMPLE_1), todo!());
+        assert_eq!(part_1(EXAMPLE_1), 1320);
     }
 
     #[test]
     fn part_2_example() {
-        assert_eq!(part_2(EXAMPLE_1), todo!());
+        assert_eq!(part_2(EXAMPLE_1), 145);
     }
 }
